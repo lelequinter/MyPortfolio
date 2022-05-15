60abb6de8defad6518f7f3017c0ec5397d15cfa3
Leyder Quintero <lele.quinter@gmail.com>
2022-05-15 12:24:48 -0500

    cambios en products

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -9,7 +9,7 @@ const Product = ({ img, link }) => {
         <div className="p-circle"></div>
         <div className="p-circle"></div>
       </div>
-      <a href="http://lelequinter.com" target="_blank" rel="noreferrer">
+      <a href={link} target="_blank" rel="noreferrer">
         <img src={img} alt="" className="p-img" />
       </a>
     </div>
