c4e2a7923dd25346534e09ad1c6df8f731e0281b
Leyder Quintero <lele.quinter@gmail.com>
2022-05-23 13:52:57 -0500

    Actualizada la Data

--- src/data.js
+++ src/data.js
@@ -1,20 +1,57 @@
-import gifApp from "./img/GifApp.png"
-import test from "./img/test.png"
+import gifApp from "./img/GifApp.png";
+import MDB from "./img/MDbTest.png";
+import RecipiesApp from "./img/RecipiesApp.png";
+import FormInventiba from "./img/FormInventiba.png";
+import MarvelApp from './img/MarvelApp.png'
+import JavaCal from './img/JavaCal.png'
+import BatteryLevel from './img/BatteryLevel.png'
+import IotEsp from './img/IotEsp.png'
+import Coursera from './img/Coursera.png'
 
 export const products = [
-    {
-        id: 1,
-        img: gifApp,
-        link: ""
-    },
-    {
-        id: 2,
-        img: test,
-        link: ""
-    },
-    {
-        id: 3,
-        img: gifApp,
-        link: ""
-    }
-]
+  {
+    id: 1,
+    img: MDB,
+    link: "",
+  },
+  {
+    id: 2,
+    img: RecipiesApp,
+    link: "",
+  },
+  {
+    id: 3,
+    img: FormInventiba,
+    link: "",
+  },
+  {
+    id: 4,
+    img: MarvelApp,
+    link: "",
+  },
+  {
+    id: 5,
+    img: Coursera,
+    link: "",
+  },
+  {
+    id: 6,
+    img: IotEsp,
+    link: "",
+  },
+  {
+    id: 7,
+    img: gifApp,
+    link: "",
+  },
+  {
+    id: 8,
+    img: JavaCal,
+    link: "",
+  },
+  {
+    id: 9,
+    img: BatteryLevel,
+    link: "",
+  },
+];
