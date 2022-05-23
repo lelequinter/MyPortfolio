12ee8d0593136d220a7ccdaaa56969c87b9a94f6
Leyder Quintero <lele.quinter@gmail.com>
2022-05-23 14:35:20 -0500

    Cambios en Skills

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import webdev from "../../img/WebDev.jpg";
-import MyPicAbout from '../../img/MyPicAbout.jpg'
+import MyPicAbout from "../../img/MyPicAbout.jpg";
 import Tilt from "react-vanilla-tilt";
 import "./about.css";
+import { ThemeContext } from "../../context/context";
 
 const About = () => {
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
+
   return (
     <>
       <div className="a" id="about">
@@ -54,18 +58,90 @@ const About = () => {
         <div className="a-rigth">
           <h1>My Skills</h1>
           <div className="a-skills">
-            <li>React</li>
-            <li>Bootstrap</li>
-            <li>Html</li>
-            <li>Css</li>
-            <li>JavaScript</li>
-            <li>TypeScript</li>
-            <li>Git</li>
-            <li>Postman</li>
-            <li>Styled Components</li>
-            <li>Framer Motion</li>
-            <li>React Reveal</li>
-            <li>React Spring</li>
+            <li
+              style={{
+                border: darkMode ? "1px solid #fff" : "1px solid #333",
+              }}
+            >
+              React
+            </li>
+            <li
+              style={{
+                border: darkMode ? "1px solid #fff" : "1px solid #333",
+              }}
+            >
+              Bootstrap
+            </li>
+            <li
+              style={{
+                border: darkMode ? "1px solid #fff" : "1px solid #333",
+              }}
+            >
+              Html
+            </li>
+            <li
+              style={{
+                border: darkMode ? "1px solid #fff" : "1px solid #333",
+              }}
+            >
+              Css
+            </li>
+            <li
+              style={{
+                border: darkMode ? "1px solid #fff" : "1px solid #333",
+              }}
+            >
+              JavaScript
+            </li>
+            <li
+              style={{
+                border: darkMode ? "1px solid #fff" : "1px solid #333",
+              }}
+            >
+              TypeScript
+            </li>
+            <li
+              style={{
+                border: darkMode ? "1px solid #fff" : "1px solid #333",
+              }}
+            >
+              Git
+            </li>
+            <li
+              style={{
+                border: darkMode ? "1px solid #fff" : "1px solid #333",
+              }}
+            >
+              Postman
+            </li>
+            <li
+              style={{
+                border: darkMode ? "1px solid #fff" : "1px solid #333",
+              }}
+            >
+              Styled Components
+            </li>
+            <li
+              style={{
+                border: darkMode ? "1px solid #fff" : "1px solid #333",
+              }}
+            >
+              Framer Motion
+            </li>
+            <li
+              style={{
+                border: darkMode ? "1px solid #fff" : "1px solid #333",
+              }}
+            >
+              React Reveal
+            </li>
+            <li
+              style={{
+                border: darkMode ? "1px solid #fff" : "1px solid #333",
+              }}
+            >
+              React Spring
+            </li>
           </div>
         </div>
       </div>
